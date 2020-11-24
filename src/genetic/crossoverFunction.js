ce3d7f4e3f2f3e8b94e0504f59bbb046194345d3
Bartosz Maziarka <bahtek.maziarka@gmail.com>
2020-11-24 22:15:20 +0100

    Changed crossover function to two-point crossover

--- src/genetic/crossoverFunction.js
+++ src/genetic/crossoverFunction.js
@@ -1,6 +1,10 @@
 export default function crossoverFunction(phenotypeA, phenotypeB) {
-    const index = Math.round(Math.random() * phenotypeA.length);
-    const phenotypeAB = phenotypeA.slice(0, index).concat(phenotypeA.slice(index));
-    const phenotypeBA = phenotypeB.slice(0, index).concat(phenotypeB.slice(index));
+    let index = Math.round(Math.random() * phenotypeA.length);
+    let index2 = Math.round(Math.random() * phenotypeA.length);
+    if (index < index2) {
+        [index2, index] = [index, index2]
+    }
+    const phenotypeAB = phenotypeA.slice(0, index).concat(phenotypeB.slice(index, index2)).concat(phenotypeA.slice(index2));
+    const phenotypeBA = phenotypeB.slice(0, index).concat(phenotypeA.slice(index, index2)).concat(phenotypeB.slice(index2));
     return [phenotypeAB, phenotypeBA]
 }
